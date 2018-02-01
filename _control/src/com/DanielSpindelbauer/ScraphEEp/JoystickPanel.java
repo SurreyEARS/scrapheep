65da1a934d6d91bbdc6849fd3d36736f9d68aad4
Phil <phil.bladen@gmail.com>
2018-02-01 12:17:01 +0000

    Test commit

--- _control/src/com/DanielSpindelbauer/ScraphEEp/JoystickPanel.java
+++ _control/src/com/DanielSpindelbauer/ScraphEEp/JoystickPanel.java
@@ -25,7 +25,7 @@ public class JoystickPanel extends JPanel implements ChangeListener
 		setEnabled(false);
 		joystick.setPreferredSize(new Dimension(300, 300));
 		joystick.addChangeListener(this);
-		add(joystick, BorderLayout.CENTER);
+		add(joystick, BorderLayout.CENTER);//test
 
 		lblPosition = new JLabel("position");
 		add(lblPosition, BorderLayout.SOUTH);
