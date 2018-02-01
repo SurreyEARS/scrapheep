81d88411937abb1c38a2ca0db2bc2624f8b97b62
Phil <Phil@PJB-DESKTOP>
2018-02-01 12:05:55 +0000

    Added library functions to connect via UDP and recieve data

--- lib/ScraphEEp-2K18/scrapheep.cpp
+++ lib/ScraphEEp-2K18/scrapheep.cpp
@@ -1,52 +1,116 @@
-/*
-  Test.h - Test library for Wiring - implementation
-  Copyright (c) 2006 John Doe.  All right reserved.
-*/
+#define _HOME //!!!
 
-// include core Wiring API
-#include "WProgram.h"
+#include "scrapheep.h"
 
-// include this library's description file
-#include "Test.h"
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 
-// include description files for other libraries used (if any)
-#include "HardwareSerial.h"
+#ifndef _HOME
+const char* wifi_ssid = "EARS_LANDER_2.4";
+const char* wifi_password = "lunar-rover";
+#else
 
-// Constructor /////////////////////////////////////////////////////////////////
-// Function that handles the creation and setup of instances
+#endif
 
-Test::Test(int givenValue)
+void ESPControl::init(void)
 {
-  // initialize this instance's variables
-  value = givenValue;
+	replyPacket[0] = 'R';
+	replyPacket[1] = '\0';
 
-  // do whatever is required to initialize the library
-  pinMode(13, OUTPUT);
-  Serial.begin(9600);
+	Serial.begin(115200);
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+
+	pinMode(PIN_MOTOR_A_DIR, OUTPUT);
+	pinMode(PIN_MOTOR_A_SPEED, OUTPUT);
+	pinMode(PIN_MOTOR_B_DIR, OUTPUT);
+	pinMode(PIN_MOTOR_B_SPEED, OUTPUT);
+
+	delay(10);
+
+	Serial.print("\n\nConnecting to ");
+	Serial.println(wifi_ssid);
+
+	WiFi.begin(wifi_ssid, wifi_password);
+
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+
+		if (WiFi.status() == WL_NO_SSID_AVAIL)
+		{
+			Serial.printf("\nCannot find %s! Are you sure it is the correct name?", wifi_ssid);
+			while (true)
+				yield();
+		}
+		else if (WiFi.status() == WL_CONNECT_FAILED)
+		{
+			Serial.printf("\nFailed to connect to %s! Have you got the correct name and password?", wifi_ssid);
+			while (true)
+				yield();
+		}
+	}
+
+	Serial.println("");
+	Serial.println("WiFi connected.");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+
+	Udp.begin(localUdpPort);
+	Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 }
 
-// Public Methods //////////////////////////////////////////////////////////////
-// Functions available in Wiring sketches, this library, and other libraries
-
-void Test::doSomething(void)
+void ESPControl::processPacket(void)
 {
-  // eventhough this function is public, it can access
-  // and modify this library's private variables
-  Serial.print("value is ");
-  Serial.println(value);
+	int packetSize = Udp.parsePacket();
+	if (packetSize)
+	{
+		Serial.printf("Received %d bytes ", packetSize);
 
-  // it can also call private functions of this library
-  doSomethingSecret();
-}
+		if (packetSize == 1)
+		{
+			Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+			Udp.write(replyPacket);
+			Udp.endPacket();
+		}
 
-// Private Methods /////////////////////////////////////////////////////////////
-// Functions only available to other functions in this library
+		int bytesRead = Udp.read(incomingPacketData, packetSize);
+		Serial.print(" packet contents: {");
+		for (int i = 0; i < bytesRead; i++)
+			Serial.printf("%d%s", incomingPacketData[i], i < 5 ? ", " : "");
+		Serial.print("}\n");
 
-void Test::doSomethingSecret(void)
-{
-  digitalWrite(13, HIGH);
-  delay(200);
-  digitalWrite(13, LOW);
-  delay(200);
+		digitalWrite(LED_BUILTIN, LOW);
+		delay(1);
+		digitalWrite(LED_BUILTIN, HIGH);
+
+		digitalWrite(PIN_MOTOR_A_DIR, incomingPacketData[0] < 128);
+		digitalWrite(PIN_MOTOR_B_DIR, incomingPacketData[1] < 128);
+
+		byte motorASpeed = incomingPacketData[0];
+		motorASpeed &= ~(1 << 1);
+
+		byte motorBSpeed = incomingPacketData[1];
+		motorBSpeed &= ~(1 << 1);
+
+		int motorA = motorASpeed * 14;
+		int motorB = motorBSpeed * 14;
+
+		if (motorA > 1000) {
+			motorA = 1000;
+		}
+		if (motorB > 1000) {
+			motorB = 1000;
+		}
+
+		analogWrite(PIN_MOTOR_A_SPEED, motorA);
+		analogWrite(PIN_MOTOR_B_SPEED, motorB);
+
+		Serial.printf("Motor A speed: %d, %d\n", motorA, incomingPacketData[0] < 128);
+		Serial.printf("Motor B speed: %d, %d\n", motorB, incomingPacketData[1] < 128);
+	}
 }
 
--- lib/ScraphEEp-2K18/scrapheep.h
+++ lib/ScraphEEp-2K18/scrapheep.h
@@ -1,22 +1,40 @@
-// ensure this library description is only included once
 #ifndef scrapheep_h
 #define scrapheep_h
 
-// include types & constants of Wiring core API
-#include "WConstants.h"
+#include <WiFiUdp.h>
+
+#define PIN_MOTOR_A_DIR    0
+#define PIN_MOTOR_A_SPEED  5
+#define PIN_MOTOR_B_DIR    2
+#define PIN_MOTOR_B_SPEED  4
+
+#define PIN_D0	16
+#define PIN_D1	5
+#define PIN_D2	4
+#define PIN_D3	0
+#define PIN_D4	2
+#define PIN_D5	14
+#define PIN_D6	12
+#define PIN_D7	13
+#define PIN_D8	15
+
+extern const char* wifi_ssid;
+extern const char* wifi_password;
 
 // library interface description
-class Test
+class ESPControl
 {
   // user-accessible "public" interface
   public:
-    Test(int);
-    void doSomething(void);
+	void init(void);
+	void processPacket(void);
 
   // library-accessible "private" interface
   private:
-    int value;
-    void doSomethingSecret(void);
+	WiFiUDP Udp;
+	uint16_t localUdpPort = 4210;
+	uint8_t incomingPacketData[7];
+	char replyPacket[2];
 };
 
 #endif
